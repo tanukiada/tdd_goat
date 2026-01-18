85e5610325a78d03c77968dc87d3690377b51c32
ada taylor <admin@adataylor.io>
2026-01-18 02:13:15 -0600

    refactor homepage view to use a template

--- TDD_Goat/tests/functional_tests.py
+++ TDD_Goat/tests/functional_tests.py
@@ -1,5 +1,9 @@
 import unittest
+import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -15,11 +19,32 @@ class NewVisitorTest(unittest.TestCase):
 
         # she notices the page title and header mention to-do lists
         self.assertIn("To-Do", self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn("To-Do", header_text)
 
         # she is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
+
+        # she types "Buy peacock feathers" into a text box
+        # (Edith's hobby is tying fly-fishing lures)
+        inputbox.send_keys("Buy peacock feathers")
+
+        # when she hits enter, the page updates, and now the page lists
+        # "1: buy peacock feathers" as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertTrue(any(row.text == "1: Buy peacock feathers" for row in rows))
+
+        # There is still a text box inviting her to add another item.
+        # She enters "Use peacock feathers to make a fly"
+        # (Edith is very methodical)
         self.fail("Finish the test!")
 
-        # satisfied, she goes back to sleep
+        # The page updates again, and now shows both items on her list
 
 if __name__ == '__main__':
     unittest.main()
--- lists/tests.py
+++ lists/tests.py
@@ -4,8 +4,10 @@ from lists.views import home_page
 
 # Create your tests here.
 class HomePageTest(TestCase):
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
        response = self.client.get('/')
-       self.assertContains(response, '<title>To-Do lists</title>')
-       self.assertContains(response, "<html>")
-       self.assertContains(response, "</html>")
+       self.assertTemplateUsed(response, 'home.html')
+
+    def test_renders_homepage_content(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'To-Do')
